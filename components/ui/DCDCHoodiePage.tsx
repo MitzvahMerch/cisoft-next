4be251a924ce19acceee6095a9db58dbb889dbb9
Aaron Michaels <potomacimprints@gmail.com>
2024-10-29 16:16:49 -0400

    aa

--- components/ui/DCDCHoodiePage.tsx
+++ components/ui/DCDCHoodiePage.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import { Card, CardContent } from '@/components/ui/card';
 import { Plus, Minus, ShoppingCart } from 'lucide-react';
