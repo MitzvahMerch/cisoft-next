46e79f6f727e3833939a729b9a482ca861e6ff14
Aaron Michaels <potomacimprints@gmail.com>
2024-10-29 12:09:41 -0400

    aa

--- components/ui/ProductGallery.tsx
+++ components/ui/ProductGallery.tsx
@@ -164,7 +164,7 @@ const ProductGallery = () => {
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {products.map((product) => (
-  <Link href={`/${product.slug}`} key={product.id} className="block"> {/* Removed /pages/ from path */}
+  <Link href={`/components/ui/${product.slug}`} key={product.id} className="block">
     <Card className="cursor-pointer hover:shadow-lg transition-shadow duration-200 bg-white h-full flex flex-col">
       <CardContent className="p-4 relative flex-grow flex flex-col items-center"> {/* Added flex and centering */}
         <div className="w-full h-64 relative overflow-hidden flex items-center justify-center"> {/* Added flex centering */}
